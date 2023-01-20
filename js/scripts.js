6b102aa9c2eb044df3975386179a3c225149fbb2
Mitchell Gantz <mgantz22@icloud.com>
2023-01-20 13:17:27 -0800

    wipe js and start fresh, new outlook after lunch

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,6 @@
-//Utility Logic 
-window.onload = initialize
+//Business logic
 
-function initialize() {
-    let form = document.querySelector("form")
-    form.onsubmit = takeForm
-}
 
-//Business Logic
-
-function takeForm(event) {
-    event.preventDefault()
-    let userNumber = (document.getElementById('userNumber').value)
- 
-    //let newUserNumber = makeNumberArray(userNumber)
-    displayArray(userNumberstring)
-}
-
-function makeNumberArray(userNumber) {
-    const array = [1,2,3];
-    const array = userNumber.split("")
-    for(let i = 0; i <userNumber.length; i++) {
-    if(array.includes(array[i])){
-        array[i] = []
-    }  
-    }
-return array
-}
 
 //User Interface Locic 
 
-function displayNumberArray(array) {
-    let outputSpot = document.getElementById("outputSpot")
-    outputSpot.innerText = ""
-    outputSpot.append(numberArray)
-}
