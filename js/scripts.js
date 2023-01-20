933eeec7f90be9c000d3d004123036ca704361bb
Mitchell Gantz <mgantz22@icloud.com>
2023-01-20 15:18:16 -0800

    wipe js and start over, envoke stress

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,19 @@
 //Business logic
+window.onload = initialize
+
+function initialize(){
+    let form = document.querySelector("userform")
+    form.onsubmit = takeForm
+}
 
 
+function takeform(event) {
+    event.prevenDefault();
+    let input = parseInt(document.querySelector("number").value)
+    let numberArray = makeNumberArray(input)
+    displayArray(numberArray)
+}
 
+   
 //User Interface Locic 
 
