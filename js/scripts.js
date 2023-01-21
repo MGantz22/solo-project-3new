0624e174fd7242c8dc943686859c87030503bfef
Mitchell Gantz <mgantz22@icloud.com>
2023-01-20 17:26:00 -0800

    add readme

--- js/scripts.js
+++ js/scripts.js
@@ -4,24 +4,26 @@ function robsTranslator(event) {
     let input = parseInt(document.querySelector("input#number").value)
     let numbers = []
 
-    for (let i = 0; i <= input; i += 1) {
-        let iString = i.toString();
-        if (iString.includes(3)){
-            numbers.push(" 'Won't you be my neighbor?'");}
-        else if (iString.includes(2)) {
-        numbers.push(" 'Boop!'");
+    for (let integer = 0; integer <= input; integer += 1) {
+        let integerString = integer.toString();
+
+        if (integerString.includes(3)) {
+            numbers.push(" 'Won't you be my neighbor?'");
+        }
+        else if (integerString.includes(2)) {
+            numbers.push(" 'Boop!'");
         } 
-        else if (iString.includes(1)) {
-        numbers.push(" 'Beep!'");
+        else if (integerString.includes(1)) {
+            numbers.push(" 'Beep!'");
         } 
         else {
-        numbers.push(" " + i);
+            numbers.push(" " + integer);
         }
         document.getElementById("answer").innerText = numbers;
 }
   //User Interface Logic
 }
 window.addEventListener("load", function() {
-const form = document.getElementById("question");
-form.addEventListener("submit", robsTranslator);
+    const form = document.getElementById("question");
+    form.addEventListener("submit", robsTranslator);
 }); 
---
+++ readme.md
@@ -0,0 +1,39 @@
+# _Indepedent Project #3:_
+
+#### Contributors: 
+
+#### _A page demonstrating new skills in JavaScript and html_
+
+
+## Technologies Used
+
+* _HTML_
+* _CSS_
+* _Markdown_
+* _JavaScript_
+
+
+## Description
+_This is a project showcasing the skills I've learned so far in the course at Epicodus; This is my Third solo project. On this page you will learn what your destiny is as a programmer and a recommendation on what language is best for you depending on your answers. You will also find a list of my first projects along with links._
+
+## Tests
+
+
+
+
+
+## Setup/Installation Requirements
+
+* _Clone this repository to your desktop_
+* _Navigate to the top level of the directory_
+* _Open index.html in your browser_
+
+## Known Bugs
+
+* text
+
+## License
+
+_MIT_
+
+Copyright (c) _2023_ _Mitchell Gantz_
